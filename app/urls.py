9762427fea30807ac7542ce2245ddce3a890d43c
jadliaissam-edu <jadliaissam.edu@gmail.com>
2026-04-01 08:50:49 +0100

    add contact form

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+
 from app import views
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
-    #path('hello', views.hello),
-    path('form', views.handle_form),
+    # path('hello', views.hello),
+    path('form', views.show_contact_form),
+    path('process_form', views.handle_contact_form),
     path('messages', views.list_messages),
     path('product', views.product_count),
     path('product/nouveau', views.show_form),
